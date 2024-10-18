41ab34ed9c8abc1b9ecc7955768db25146bc848a
hemgarg <hemgarg23@gmail.com>
2024-10-18 17:00:30 +0530

    initial commit

---
+++ Lesson-1 Activity 1/StoringBirthday's.py
@@ -0,0 +1,41 @@
+import time
+
+time.sleep(2) # add a 2-second delay
+
+#Storing Nihal's Birthday
+NihalBirthday = input ("What is your Birthday Nihal? (dd.mm.yyyy)")
+
+time.sleep(2) # add a 2-second delay
+
+#Printing his Birthday
+print ("So your birthday is" ,NihalBirthday)
+
+time.sleep(2) # add a 2-second delay
+
+#Storing Ayaan's birthday
+AyaanBirthday = input ("What is your Birthday Ayaan? (dd.mm.yy)")
+
+time.sleep(2) # add a 2-second delay
+
+#Printing his Birthday
+print ("So your birthday is" ,AyaanBirthday)
+
+time.sleep(2) # add a 2-second delay
+
+#Storing Aarav's Birthday
+AaravBirthday = input ("What is your Birthday Aarav? (dd.mm.yyyy)")
+
+time.sleep(2) # add a 2-second delay
+
+#Printing his Birthday
+print ("So your birthday is" ,AaravBirthday)
+
+time.sleep(2) # add a 2-second delay
+
+#Storing My sister's Birthday
+AaraviBirthday = input ("What is your Birthday Aaravi? (dd.mm.yyyy)")
+
+time.sleep(2) # add a 2-second delay
+
+#Printing her Birthday
+print ("So your birthday is", AaraviBirthday)
