026094306cd8fa630e3bec4de42160afc7de6c84
hemgarg <hemgarg23@gmail.com>
2024-12-11 16:09:26 +0530

    Break

---
+++ Lesson-1 Activity 1/Break.py
@@ -0,0 +1,10 @@
+#take input
+a = input ("Enter a word:")
+#program to check break keyword
+for i in a:
+    if (i == 'A'):
+        #display result
+        print ("A is found")
+        break
+    else:
+        print("A not found")
