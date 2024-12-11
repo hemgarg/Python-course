5a3532edf74431fe3d73b59cc7fa56ab20360e68
hemgarg <hemgarg23@gmail.com>
2024-12-11 16:22:31 +0530

    Due_hw

---
+++ Lesson-1 Activity 1/Due_hw.py
@@ -0,0 +1,5 @@
+Bill = int(input("Please enter the amount the bill was"))
+Paid = int(input("Please enter the amount the customer gave"))
+
+Due = Paid - Bill
+print("So the shopkeer has to give back $",Due)
