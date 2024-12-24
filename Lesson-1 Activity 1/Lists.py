5f5192d2ea9923cf024d3bcf0cea7c4a37988118
hemgarg <hemgarg23@gmail.com>
2024-12-24 21:06:34 +0530

    Lists

---
+++ Lesson-1 Activity 1/Lists.py
@@ -0,0 +1,16 @@
+#create an exmpty list
+empty_list = []
+print()
+
+#a list of number
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+#use * operator
+triples = [1, 2, 3] * 3 
+print(triples)
+
+#reverse el given list
+aList = [100, 200, 300, 400, 500]
+alist = aList[::-1]
+print(alist,"\n")
