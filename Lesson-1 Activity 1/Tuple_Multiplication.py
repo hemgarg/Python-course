54c9099e46cd07c81d8033f795dac45c5e9cac77
hemgarg <hemgarg23@gmail.com>
2024-12-31 18:05:16 +0530

    Get_rid_of_Duplicates

---
+++ Lesson-1 Activity 1/Tuple_Multiplication.py
@@ -0,0 +1,25 @@
+us = int(input("Please enter the first number"))
+us2 = int(input("Please enter the second number"))
+us3 = int(input("Also please enter the number we are going to multiply with"))
+
+us = (us)
+us2 = (us2)
+
+if us > us2:
+    print("Sorry but this is the wrong order! Swapping them..")
+    us == us2
+    us2 == us
+
+X = int(us)
+Y = int(us2)
+print("The numbers in your list are..")
+while X <= Y:
+    print(X)
+    X = X + 1
+
+print("The product of these numbers are...")
+Z = (us)
+for i in range(Y + 1 - us):
+    sqr = (Z * us3)
+    print(sqr)
+    Z = Z + 1
