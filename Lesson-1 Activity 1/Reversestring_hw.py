c6c2e469638fd7a3ddcdc7d979b6c7b226fc10dd
hemgarg <hemgarg23@gmail.com>
2025-02-05 17:53:16 +0530

    Reverse_string_hw

--- Lesson-1 Activity 1/Reversestring_hw.py
+++ Lesson-1 Activity 1/Reversestring_hw.py
@@ -1,26 +1,21 @@
 class GetReverseString:
-    def __init__(self,str1):
+    def __init__(self, str1):
         self.str1 = str1
-        self.str2 = []
-    def Reverse(self,str1):
-        self.str1 = list(str1)
-        x = len(self.str1)
-        print(x)
-        str2 = []
-        for i in range (x):
-            s = len(str1) - 1
-            print(s)
-            str2.append(s)
-            s -= 1
-    def display(str2):
-        print("Reversed str is --",str2)
+        self.str2 = ""
+    
+    def Reverse(self):
+        # Reverse the string using slicing
+        self.str2 = self.str1[::-1]
+    
+    def display(self):
+        print("Reversed str is --", self.str2)
 
 
-temp = input("Please enter the thing to be reversed")
+temp = input("Please enter the thing to be reversed: ")
 userinput = GetReverseString(temp)
-
-userinput.Reverse(userinput.str1)
+userinput.Reverse()
 userinput.display()
+
 
 
             
