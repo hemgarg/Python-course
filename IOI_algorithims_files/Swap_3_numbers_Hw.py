7cd72580be0a3aa5e32ba90113e9fcb9227af350
hemgarg <hemgarg23@gmail.com>
2025-04-09 17:30:37 +0530

    Swap_3_Numbers

---
+++ IOI_algorithims_files/Swap_3_numbers_Hw.py
@@ -0,0 +1,10 @@
+num1 = input("Enter value of num1")
+num2 = input("Enter value of num2")
+num3 = input("Enter value of num3")
+
+temp = num1
+num1 = num2
+num2 = num3
+num3 = temp
+
+print("Value of num1, num2 and num3 respectively after swapping are", num1, num2,num3)
