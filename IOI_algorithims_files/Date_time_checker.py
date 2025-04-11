ee4ec402be28c3b1947c3f041e31be16d2a0d025
hemgarg <hemgarg23@gmail.com>
2025-04-11 17:19:44 +0530

    Date_time_Checker

---
+++ IOI_algorithims_files/Date_time_checker.py
@@ -0,0 +1,9 @@
+import datetime
+
+current_time = datetime.datetime.now()
+print("TTime  row at greenwich meridain is :", end = "")
+print(current_time)
+
+import calendar
+print("\n",calendar.calendar(2021))
+
