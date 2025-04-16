222b6f5ffadd6976314d9f5f4a9a90449738b242
hemgarg <hemgarg23@gmail.com>
2025-04-16 17:07:13 +0530

    Table_of_23

--- IOI_algorithims_files/Date_time_checker.py
+++ IOI_algorithims_files/Date_time_checker.py
@@ -1,9 +1,8 @@
 import datetime
 
 current_time = datetime.datetime.now()
-print("TTime  row at greenwich meridain is :", end = "")
+print("Time  row at greenwich meridain is :", end = "")
 print(current_time)
 
 import calendar
 print("\n",calendar.calendar(2021))
-
