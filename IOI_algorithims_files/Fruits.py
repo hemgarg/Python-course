42186967ca79fe8455d068407c168844e3190bef
hemgarg <hemgarg23@gmail.com>
2025-04-25 17:15:18 +0530

    Fruits

---
+++ IOI_algorithims_files/Fruits.py
@@ -0,0 +1,28 @@
+lst = ["Apple", "Guava", "Mango", "Banana","Kiwi"]
+print("Length of list", len(lst))
+print("First element:", lst[0])
+print("Last element:", lst[-1])
+
+lst.append("Papaya")
+print("Updated list:",lst)
+
+lst.remove("Guava")
+print("Updated list:",lst)
+
+lst.sort()
+print("Sorted list",lst)
+
+
+lst.pop(1)
+print("Updated list:",lst)
+
+lst.reverse()
+print("Reversed list:",lst)
+
+print("Multiplication on list:",lst*2)
+
+lst = lst[:4]
+print("Sliced list:",lst)
+
+lst.clear()
+print("Updated list:",lst)
---
+++ IOI_algorithims_files/polygons_hw.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.Screen().bgcolor("#4876B7")
+q = turtle.Turtle()
+
+q.hideturtle()
+q.speed("slowest")
+for i in range(3):
+    q.forward()
+
+turtle.done()
