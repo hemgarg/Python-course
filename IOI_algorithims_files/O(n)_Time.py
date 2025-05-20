3ea86c0eefc9888fb8902f13fbe72ef0fb7de192
hemgarg <hemgarg23@gmail.com>
2025-05-20 17:19:59 +0530

    O(n)_Time

--- IOI_algorithims_files/O(n)_Time.py
+++ IOI_algorithims_files/O(n)_Time.py
@@ -2,7 +2,7 @@ def OnTime(n):
     iteration = 0
     for i in range(1,n+1):
         iteration += 1
-    print("When n is",n,"Iterations = ",iterations)
+    print("When n is",n,"Iterations = ",iteration)
 
 OnTime(10)
 OnTime(20)
