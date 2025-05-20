077e043be82b36959acc3ed12f3b5a196a5b5cf3
hemgarg <hemgarg23@gmail.com>
2025-05-20 17:13:15 +0530

    O(1)_Time

---
+++ IOI_algorithims_files/O(1)_Time.py
@@ -0,0 +1,10 @@
+def printnumber(n):
+    iteration = 0
+    print(f"The number entered by the user is {n}")
+    iteration = 1
+    print("Total iterations done by the code is",iteration)
+
+printnumber(10)
+printnumber(20)
+
+print("\n With any 'n' the time taken by our code wont change ")
