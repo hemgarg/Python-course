45744139033bc5c699072383f25abe817d82380c
hemgarg <hemgarg23@gmail.com>
2025-05-20 17:18:30 +0530

    O(n)_Time

---
+++ IOI_algorithims_files/O(n)_Time.py
@@ -0,0 +1,11 @@
+def OnTime(n):
+    iteration = 0
+    for i in range(1,n+1):
+        iteration += 1
+    print("When n is",n,"Iterations = ",iterations)
+
+OnTime(10)
+OnTime(20)
+OnTime(42)
+
+print("\n With every 'n' the time taken and iteration wiill increase")
