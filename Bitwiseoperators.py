e12891489a144bf9752339d8561bd2921c6edf74
hemgarg <hemgarg23@gmail.com>
2025-05-31 12:01:27 +0530

    Bitwise_operators

---
+++ Bitwiseoperators.py
@@ -0,0 +1,33 @@
+num1 = 10
+num2 = 4
+
+#Using And operators
+print("num1 & num2 = ", num1 & num2)
+
+#Using or operators
+print("\n num1 | num2 = ", num1 | num2)
+
+#Using not operators
+print("\n ~num 1 = ", ~num1)
+
+#Using xor operator
+print("\n num1 ^ num2 = ", num1 ^ num2)
+
+
+num1 = 10
+num2 = 4
+
+#Using Right shift operator on num1
+print("\n num1 >> 1 = ", num1 >> 1)
+
+#Using Right shift operator on num2
+print("\n num2 >> 1 = ", num2 >> 1)
+
+num1 = 10
+num2 = 4
+
+#Using Left shift operator on num1
+print("\n num1 << 1 = ", num1 << 1)
+
+#Using Left shift operator on num2
+print("\n num2 << 1 = ", num2 << 1)
