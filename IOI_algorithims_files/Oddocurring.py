b7bb0387715a6c2c8febc16ceeb58b4a30d2f2ff
hemgarg <hemgarg23@gmail.com>
2025-06-20 17:10:51 +0530

    One_odd_ocurring

---
+++ IOI_algorithims_files/Oddocurring.py
@@ -0,0 +1,14 @@
+def OddOcurring(arr):
+    res = 0
+    for element in arr:
+        res = res ^ element
+    return res
+arr = []
+n = int(input("Enter array size"))
+
+while(n):
+    num = int(input("Enter number"))
+    arr.append(num)
+    n-=1
+
+print("\n\nOdd occurring number is",OddOcurring(arr))
